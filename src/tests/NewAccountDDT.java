81e83a6a106f06a3dac1a32f1efa9af7b12ee1bc
Harland Gunn <harland.j.gunn@ey.com>
2020-02-28 14:14:31 -0500

    updating NewAccountDDT test class

--- src/tests/NewAccountDDT.java
+++ src/tests/NewAccountDDT.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.fail;
 
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -16,47 +18,29 @@ import org.openqa.selenium.support.ui.Select;
 
 @RunWith(value = Parameterized.class)
 public class NewAccountDDT {
-	String name;
-	String email;
-	String phone;
-	String gender;
-	String password;
-	String country;
-	boolean weeklyEmail;
-	boolean monthlyEmail;
-	boolean occasionalEmail;
+	String name, email, phone, gender, password, country;
+	boolean weeklyEmail, monthlyEmail, occasionalEmail;
+	WebElement nameBoxElement, emailBoxElement, phoneBoxElement, passwordBoxElement, verifyPasswordElement, countryElement, weeklyEmailElement, monthlyEmailElement, occasionalEmailElement, submitElement;
+	WebDriver driver;
 	
 	// This is our test method
 	@Test
 	public void newAccountTest() {
-		// Set WebDriver and open browser to webURL
-		String webURL = "http://sdettraining.com/trguitransactions/NewAccount.aspx";
-		WebDriver driver = utilities.DriverFactory.open("edge");
-		driver.get(webURL);
-		driver.findElement(By.xpath("html/body/form/div[3]/div[2]/div/div[2]/a")).click();
-		
+		//Print record to console
+		System.out.println("NEW RECORD: " + name + " " + email + " " + phone + " " + gender + " " + password + " " + country + " " + weeklyEmail + " " + monthlyEmail + " " + occasionalEmail);
+				
 		// Define web elements
-		WebElement nameBoxElement = driver.findElement(By.id("MainContent_txtFirstName"));
-		WebElement emailBoxElement = driver.findElement(By.id("MainContent_txtEmail"));
-		WebElement phoneBoxElement = driver.findElement(By.id("MainContent_txtHomePhone"));
-		WebElement passwordBoxElement = driver.findElement(By.id("MainContent_txtPassword"));
-		WebElement verifyPasswordElement = driver.findElement(By.id("MainContent_txtVerifyPassword"));
-		WebElement countryElement = driver.findElement(By.id("MainContent_menuCountry"));
-		WebElement weeklyEmailElement = driver.findElement(By.id("MainContent_checkWeeklyEmail"));
-		WebElement monthlyEmailElement = driver.findElement(By.id("MainContent_checkMonthlyEmail")); 
-		WebElement occasionalEmailElement = driver.findElement(By.id("MainContent_checkUpdates"));
-		WebElement submitElement = driver.findElement(By.id("MainContent_btnSubmit"));
+		defineWebElements();
+				
+		// ******-----------------------Fill out the Form------------------------------->>>>>>
 		
-		// Fill out the Form
 		nameBoxElement.sendKeys(name);
 		emailBoxElement.sendKeys(email);
 		phoneBoxElement.sendKeys(phone);
 		// Selecting gender radio button
 		if (gender.equalsIgnoreCase("male")) {driver.findElement(By.id("MainContent_Male")).click();} 
 		else {driver.findElement(By.id("MainContent_Female")).click();}
-				
-		System.out.println("NEW RECORD: " + name + " " + email + " " + phone + " " + gender + " " + password + " " + country + " " + weeklyEmail + " " + monthlyEmail + " " + occasionalEmail);
-		
+		//Entering password and verifying password
 		passwordBoxElement.sendKeys(password);
 		verifyPasswordElement.sendKeys(password);
 		// Selecting Country
@@ -90,7 +74,7 @@ public class NewAccountDDT {
 			}
 		}
 		// Submitting form
-		submitElement.click();
+//		submitElement.click();
 				 
 	}
 	
@@ -116,6 +100,37 @@ public class NewAccountDDT {
 		
 		if (occasionalEmail.equalsIgnoreCase("True")) {this.occasionalEmail = true;} 
 		else {this.occasionalEmail = false;}
+	}
+	
+	@Before
+	public void setUp() {
+		// Set WebDriver and open browser to webURL
+		String webURL = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
+		driver = utilities.DriverFactory.open("edge");
+		driver.get(webURL);
+		driver.findElement(By.cssSelector(".btn-default")).click();
+	}
+	
+	@After
+	public void tearDown() {
+		driver.close();
+		System.out.println("Closing web browser.");
+		driver.quit();
+		System.out.println("Terminating driver instance");
+	}
+	
+	public void defineWebElements() {
+		// Define web elements
+				nameBoxElement = driver.findElement(By.id("MainContent_txtFirstName"));
+				emailBoxElement = driver.findElement(By.id("MainContent_txtEmail"));
+				phoneBoxElement = driver.findElement(By.id("MainContent_txtHomePhone"));
+				passwordBoxElement = driver.findElement(By.id("MainContent_txtPassword"));
+				verifyPasswordElement = driver.findElement(By.id("MainContent_txtVerifyPassword"));
+				countryElement = driver.findElement(By.id("MainContent_menuCountry"));
+				weeklyEmailElement = driver.findElement(By.id("MainContent_checkWeeklyEmail"));
+				monthlyEmailElement = driver.findElement(By.id("MainContent_checkMonthlyEmail")); 
+				occasionalEmailElement = driver.findElement(By.id("MainContent_checkUpdates"));
+				submitElement = driver.findElement(By.id("MainContent_btnSubmit"));
 	}
 	
 }
